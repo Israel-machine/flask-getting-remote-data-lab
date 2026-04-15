154c77637c46ad0877fa7910af46dbbb408c6d3d
Israel-machine <israel.diaz1701@gmail.com>
2026-04-15 13:06:45 -0700

    json data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,27 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        #Query End-Point
+        response = requests.get(self.url)
+        if response.status_code==200:
+            return self.load_json(response.text)
+        else:
+            return None
+        
+    #CONVERTS & RETURNS END POINT DATA TO JSON 
+    def load_json(self, raw_data):
+        data = json.loads(raw_data)
+        return data
+    
 
-    def load_json(self):
-        pass
+#EXECUTION AND TESTING:
+test = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+
+# Execute the logic
+data = test.get_response_body()
+
+# Print the result
+print(data)
+
+
+
