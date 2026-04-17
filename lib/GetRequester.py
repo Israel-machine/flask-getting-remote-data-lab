f9656eae236dc6253720b610378e8a223ab44903
Israel-machine <israel.diaz1701@gmail.com>
2026-04-17 00:31:06 -0700

    refactor of get_response & load_json

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,16 +8,20 @@ class GetRequester:
 
     def get_response_body(self):
         response = requests.get(self.url)
-        if response.status_code == 200:
-            data = self.load_json(response.text)
-            
-            return json.dumps(data, indent=2).encode('utf-8')
-        else:
-            return None
+        return response.content
+        # if response.status_code == 200:
+        #     data = self.load_json(response.text)
+        #     return json.dumps(data, indent=2).encode('utf-8')
+        
+        # else:
+        #     return None
         
     @staticmethod
-    def load_json(raw_data):
-        return json.loads(raw_data)
+    def load_json():
+        response = requests.get(self.url)
+        return response.json()
+        # data = json.loads(raw_data)
+        # return data
 
 
 
