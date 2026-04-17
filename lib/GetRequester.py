eb9a5d9bed8161a582db12d154bf9520dd2eae22
Israel-machine <israel.diaz1701@gmail.com>
2026-04-17 00:42:09 -0700

    Removed comments

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -9,19 +9,12 @@ class GetRequester:
     def get_response_body(self):
         response = requests.get(self.url)
         return response.content
-        # if response.status_code == 200:
-        #     data = self.load_json(response.text)
-        #     return json.dumps(data, indent=2).encode('utf-8')
+    
         
-        # else:
-        #     return None
-        
-    # @staticmethod
     def load_json(self):
         response = requests.get(self.url)
         return response.json()
-        # data = json.loads(raw_data)
-        # return data
+       
 
 
 
